4bf537cb6101ff39fa0467ec2b79b3f15d9ff637
Aliaksandr Rabushka <aliaksandr.rabushka@fingers.by>
2023-01-25 00:19:22 +0100

    feat(redis): add redis store

---
+++ Internal/infrastructure/repository/redis_store/redis_store.go
@@ -0,0 +1,43 @@
+package redis_store
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"proxy_map/Internal/domain/models"
+)
+
+type ProxyRedisStore struct {
+	Client *redis.Client
+}
+
+func NewProxyRedisStore(ctx context.Context, redisAddr string) (*ProxyRedisStore, error) {
+	// TODO: Move NewClient to app
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "",
+		DB:       0,
+	})
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+	return &ProxyRedisStore{
+		Client: client,
+	}, nil
+}
+
+func (s *ProxyRedisStore) Save(k *models.ProxyRequest, v *models.ProxyResponse) error {
+
+	//err := s.Client.Do(context.TODO(), k, v).Err()
+
+	return nil
+}
+
+func (s *ProxyRedisStore) Get(k *models.ProxyRequest) (*models.ProxyResponse, error) {
+
+	return nil, nil
+}
+
+func (s *ProxyRedisStore) All() (map[*models.ProxyRequest]*models.ProxyResponse, error) {
+
+	return nil, nil
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module proxy_map
 go 1.18
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/redis/go-redis/v9 v9.0.0-rc.4 // indirect
 )
